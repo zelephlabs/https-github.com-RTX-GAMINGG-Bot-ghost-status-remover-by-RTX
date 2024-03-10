6bdcd7fa1d470bdd403f8ac4407bbabd785c7b78
Gabriel7646 <123839438+Gabriel7646@users.noreply.github.com>
2024-03-10 21:24:57 +0200

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 let currentIndex = 0;
 const channelId = '';
 
-const statusMessages = ["Made By Gabriel","Watching You","Made By Gabe"];
+const statusMessages = ["Watching Youtube","listening to Spotify"];
 
 async function login() {
   try {
