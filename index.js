2136ed08edd8730da1a71c92371614644edea19b
Gabriel7646 <123839438+Gabriel7646@users.noreply.github.com>
2024-03-10 19:41:59 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Made By Gabriel","Made By Gabe"];
+const statusMessages = ["Made By Gabriel","Made By Gabee"];
 
 
 let currentIndex = 0;
