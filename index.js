b8d2398519fcb4dbd703be1bb1a0e2cd7e4a9552
Gabriel7646 <123839438+Gabriel7646@users.noreply.github.com>
2024-03-10 19:35:16 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Made By Gabriel","Made By Gabriel"];
 
 
 let currentIndex = 0;
